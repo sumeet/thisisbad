0528353c4be3244da522054a305daa7cedf37b8c
Sumeet Agarwal <sumeet.a@gmail.com>
2012-02-26 23:05:24 -0800

    ocd

--- lib/pywithrb.rb
+++ lib/pywithrb.rb
@@ -2,8 +2,7 @@ require "shell"
 
 require "json_with_scalars"
 
-class PythonError < StandardError ; end
-
+Shell.def_system_command(:pygmentize, "pygmentize -l pytb")
 
 module Python
   class Module
@@ -14,38 +13,36 @@ module Python
     def method_missing(python_function_name, *args)
       output = call_python(python_function_name, args.to_json)
       if contains_python_stack_trace?(output)
-        puts format_stack_trace(output)
+        puts colorize_stack_trace(output)
         raise PythonError
       end
       parse_output_to_ruby(output)
     end
 
     private
+    def call_python(python_function_name, args)
+      %x[python -c 'import json ; from #{@name} import * ; \
+                    args = json.loads("""#{args}""") ; \
+                    print json.dumps(#{python_function_name}(*args))' \
+                    2>&1]
+    end
+
     def contains_python_stack_trace?(output)
       output.include? "Traceback (most recent call last)"
     end
 
-    def call_python(python_function_name, args)
-      %x[python -c 'import json ; from #{@name} import * ; \
-         args = json.loads("""#{args}""") ; \
-         print json.dumps(#{python_function_name}(*args))' 2>&1]
+    def colorize_stack_trace(output)
+      Shell.new.transact { echo(output) | pygmentize }.to_s
     end
-
-    def format_stack_trace(output)
-      process = Shell.new.transact do
-        echo(output) | pygmentize
-      end
-      process.to_s
-    end
-
-    Shell.def_system_command(:pygmentize, "pygmentize -l pytb")
 
     def parse_output_to_ruby(output)
       JSONWithScalars.parse(output.strip)
     end
   end
 
-  def self.method_missing(name, *args)
+  def self.method_missing(name, *)
     Module.new(name)
   end
+
+  class PythonError < StandardError ; end
 end
--- spec/pywithrb_spec.rb
+++ spec/pywithrb_spec.rb
@@ -2,10 +2,12 @@ require 'pywithrb'
 
 describe "pywithrb" do
   before(:each) do
-    File.open("poop.py", "w") { |f| f.write("def id(x): return x") }
+    File.open("python_module.py", "w") do |f|
+      f.write("def add_one(x): return x + 1")
+    end
   end
 
-  after(:each) { File.delete("poop.py") }
+  after(:each) { File.delete("python_module.py") }
 
   it "raises an error when there's an exception" do
    output = ["Traceback (most recent call last):\e[39;49;00m\n  File ",
@@ -15,11 +17,13 @@ describe "pywithrb" do
              "\e[39;49;00m: \e[39;49;00mname 'idx' is not ",
              "defined\e[39;49;00m\n"].join("")
     STDOUT.should_receive(:puts).with(output)
-    expect { Python::poop.idx(123) }.to raise_exception(PythonError)
+    expect do
+      Python::python_module.idx(123)
+    end.to raise_exception(Python::PythonError)
   end
 
-  it "does not raise an error when there's no exception" do
-    Python::poop.id(123).should == 123
+  it "receives output from python" do
+    Python::python_module.add_one(123).should == 124
   end
 end
 
